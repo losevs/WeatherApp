64bf4fcbf6740a47b743ff534a128065f4667fc1
Sergey <hi.sergeyl@gmail.com>
2023-08-22 14:53:21 +0300

    breaking change: add handlers & json response

--- handlers/handlers.go
+++ handlers/handlers.go
@@ -1,11 +1,120 @@
 package handlers
 
 import (
+	"encoding/json"
+	"fmt"
+	"io"
 	"net/http"
+	"time"
 
 	"github.com/gin-gonic/gin"
 )
 
-func Get(context *gin.Context) {
-	context.JSON(http.StatusOK, "status ok")
+type Weather struct {
+	Location struct {
+		Name    string `json:"name"`
+		Country string `json:"country"`
+	} `json:"location"`
+	Current struct {
+		TempC     float64 `json:"temp_c"`
+		Condition struct {
+			Text string `json:"text"`
+		} `json:"condition"`
+	} `json:"current"`
+	Forecast struct {
+		Forecastday []struct {
+			Hour []struct {
+				TimeEpoch int64   `json:"time_epoch"`
+				TempC     float64 `json:"temp_c"`
+				Condition struct {
+					Text string `json:"text"`
+					//Icon string `json:"icon"`
+				} `json:"condition"`
+				ChanceOfRain float64 `json:"chance_of_rain"`
+			} `json:"hour"`
+		} `json:"forecastday"`
+	} `json:"forecast"`
+}
+
+type Response struct {
+	Time         string  `json:"time"`
+	Temp         float64 `json:"temp"`
+	ChanceOfRain float64 `json:"chance_of_rain"`
+	Condition    string  `json:"condition"`
+}
+
+type ResponseNow struct {
+	City      string  `json:"city"`
+	Counrty   string  `json:"country"`
+	Temp      float64 `json:"temperature"`
+	Condition string  `json:"condition"`
+}
+
+func GetCityNow(context *gin.Context) {
+	city, isHere := context.Params.Get("city")
+	if !isHere {
+		context.Status(http.StatusBadRequest)
+	}
+	req, err := http.Get("http://api.weatherapi.com/v1/forecast.json?key=b3eac86bb01f44ce9ae101830232208&q=" + city + "&days=1&aqi=no&alerts=no")
+	if err != nil {
+		context.JSON(http.StatusBadRequest, "Wrong city")
+	}
+	defer req.Body.Close()
+	body, err := io.ReadAll(req.Body)
+	if err != nil {
+		context.JSON(http.StatusBadRequest, "error while reading req.Body")
+	}
+	var weather Weather
+	err = json.Unmarshal(body, &weather)
+	if err != nil {
+		context.JSON(http.StatusBadRequest, fmt.Sprintf("Error while unmarshalling: %s", err))
+	}
+	location := weather.Location
+	current := weather.Current
+	response := ResponseNow{
+		City:      location.Name,
+		Counrty:   location.Country,
+		Temp:      current.TempC,
+		Condition: current.Condition.Text,
+	}
+	// weatherNow := fmt.Sprintf("%s, %s, %.0fC, %s", location.Name, location.Country, current.TempC, current.Condition.Text)
+	context.JSON(http.StatusOK, response)
+}
+
+func GetCityFuture(context *gin.Context) {
+	city, isHere := context.Params.Get("city")
+	if !isHere {
+		context.Status(http.StatusBadRequest)
+	}
+	req, err := http.Get("http://api.weatherapi.com/v1/forecast.json?key=b3eac86bb01f44ce9ae101830232208&q=" + city + "&days=1&aqi=no&alerts=no")
+	if err != nil {
+		context.JSON(http.StatusBadRequest, "Wrong city")
+	}
+	defer req.Body.Close()
+	body, err := io.ReadAll(req.Body)
+	if err != nil {
+		context.JSON(http.StatusBadRequest, "error while reading req.Body")
+	}
+	var weather Weather
+	err = json.Unmarshal(body, &weather)
+	if err != nil {
+		context.JSON(http.StatusBadRequest, fmt.Sprintf("Error while unmarshalling: %s", err))
+	}
+	//location := weather.Location
+	//current := weather.Current
+	hours := weather.Forecast.Forecastday[0].Hour
+	// weatherNow := fmt.Sprintf("%s, %s, %.0fC, %s", location.Name, location.Country, current.TempC, current.Condition.Text)
+	// context.JSON(http.StatusOK, weatherNow)
+	allResponses := []Response{}
+	for _, hour := range hours {
+		if !time.Unix(hour.TimeEpoch, 0).Before(time.Now()) {
+			var response Response
+			response.Time = time.Unix(hour.TimeEpoch, 0).Format("15:04")
+			response.Temp = hour.TempC
+			response.ChanceOfRain = hour.ChanceOfRain
+			response.Condition = hour.Condition.Text
+			allResponses = append(allResponses, response)
+		}
+	}
+	context.JSON(http.StatusOK, allResponses)
 }
--- main.go
+++ main.go
@@ -1,76 +1,71 @@
 package main
 
 import (
-	//"log"
-	//"losevs/handlers"
+	"log"
+	"losevs/handlers"
 
-	"encoding/json"
-	"fmt"
-	"io"
-	"net/http"
-	"os"
-	"time"
-	//"github.com/gin-gonic/gin"
+	"github.com/gin-gonic/gin"
 )
 
-type Weather struct {
-	Location struct {
-		Name    string `json:"name"`
-		Country string `json:"country"`
-	} `json:"location"`
-	Current struct {
-		TempC     float64 `json:"temp_c"`
-		Condition struct {
-			Text string `json:"text"`
-		} `json:"condition"`
-	} `json:"current"`
-	Forecast struct {
-		Forecastday []struct {
-			Hour []struct {
-				TimeEpoch int64   `json:"time_epoch"`
-				TempC     float64 `json:"temp_c"`
-				Condition struct {
-					Text string `json:"text"`
-					//Icon string `json:"icon"`
-				} `json:"condition"`
-				ChanceOfRain float64 `json:"chance_of_rain"`
-			} `json:"hour"`
-		} `json:"forecastday"`
-	} `json:"forecast"`
-}
+// type Weather struct {
+// 	Location struct {
+// 		Name    string `json:"name"`
+// 		Country string `json:"country"`
+// 	} `json:"location"`
+// 	Current struct {
+// 		TempC     float64 `json:"temp_c"`
+// 		Condition struct {
+// 			Text string `json:"text"`
+// 		} `json:"condition"`
+// 	} `json:"current"`
+// 	Forecast struct {
+// 		Forecastday []struct {
+// 			Hour []struct {
+// 				TimeEpoch int64   `json:"time_epoch"`
+// 				TempC     float64 `json:"temp_c"`
+// 				Condition struct {
+// 					Text string `json:"text"`
+// 					//Icon string `json:"icon"`
+// 				} `json:"condition"`
+// 				ChanceOfRain float64 `json:"chance_of_rain"`
+// 			} `json:"hour"`
+// 		} `json:"forecastday"`
+// 	} `json:"forecast"`
+// }
 
 func main() {
-	query := "Moscow"
-	if len(os.Args) >= 2 {
-		query = os.Args[1]
-	}
-	req, err := http.Get("http://api.weatherapi.com/v1/forecast.json?key=b3eac86bb01f44ce9ae101830232208&q=" + query + "&days=1&aqi=no&alerts=no")
-	if err != nil {
-		panic(err) //!
-	}
-	defer req.Body.Close()
-	if req.StatusCode != 200 {
-		panic("Weather App isn't available") //!
-	}
-	body, err := io.ReadAll(req.Body)
-	if err != nil {
-		panic(err) //!
-	}
-	var weather Weather
-	err = json.Unmarshal(body, &weather)
-	if err != nil {
-		panic(err) //!
-	}
-	location := weather.Location
-	current := weather.Current
-	hours := weather.Forecast.Forecastday[0].Hour
-	fmt.Printf("%s, %s, %.0fC, %s\n", location.Name, location.Country, current.TempC, current.Condition.Text)
-	for _, hour := range hours {
-		if !time.Unix(hour.TimeEpoch, 0).Before(time.Now()) {
-			fmt.Printf("%s - %.0fC, %.0f%%, %s\n", time.Unix(hour.TimeEpoch, 0).Format("15:04"), hour.TempC, hour.ChanceOfRain, hour.Condition.Text)
-		}
-	}
-	//router := gin.Default()
-	//router.GET("/", handlers.Get)
-	//log.Fatalln(router.Run(":80"))
+	// 	query := "Moscow"
+	// 	if len(os.Args) >= 2 {
+	// 		query = os.Args[1]
+	// 	}
+	// 	req, err := http.Get("http://api.weatherapi.com/v1/forecast.json?key=b3eac86bb01f44ce9ae101830232208&q=" + query + "&days=1&aqi=no&alerts=no")
+	// 	if err != nil {
+	// 		panic(err) //!
+	// 	}
+	// 	defer req.Body.Close()
+	// 	if req.StatusCode != 200 {
+	// 		panic("Weather App isn't available") //!
+	// 	}
+	// 	body, err := io.ReadAll(req.Body)
+	// 	if err != nil {
+	// 		panic(err) //!
+	// 	}
+	// 	var weather Weather
+	// 	err = json.Unmarshal(body, &weather)
+	// 	if err != nil {
+	// 		panic(err) //!
+	// 	}
+	// 	location := weather.Location
+	// 	current := weather.Current
+	// 	hours := weather.Forecast.Forecastday[0].Hour
+	// 	fmt.Printf("%s, %s, %.0fC, %s\n", location.Name, location.Country, current.TempC, current.Condition.Text)
+	// 	for _, hour := range hours {
+	// 		if !time.Unix(hour.TimeEpoch, 0).Before(time.Now()) {
+	// 			fmt.Printf("%s - %.0fC, %.0f%%, %s\n", time.Unix(hour.TimeEpoch, 0).Format("15:04"), hour.TempC, hour.ChanceOfRain, hour.Condition.Text)
+	// 		}
+	// 	}
+	router := gin.Default()
+	router.GET("/weather/now/:city", handlers.GetCityNow)
+	router.GET("/weather/:city", handlers.GetCityFuture)
+	log.Fatalln(router.Run(":80"))
 }
